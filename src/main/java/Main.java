bc14dc8fea5dbcd2be466cc490b51843c9928f66
Jared Hasson <jhasson@simplifile.com>
2021-06-09 12:54:46 -0600

    provide .env support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Test");
+    Dotenv dotenv = Dotenv.load();
+    String discordToken = dotenv.get("DISCORD_TOKEN");
+    System.out.println(discordToken);
   }
 }
